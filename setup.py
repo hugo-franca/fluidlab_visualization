9bb48d864dd408887d721889600c1d7add250a1f
hugo-franca <franca.hugo1@gmail.com>
2026-02-24 09:53:35 +0100

    new setup

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setuptools.setup(
             "Programming Language :: Python :: 3.10",
             "Topic :: Scientific/Engineering",
             ],
-        install_requires=["pyfonts", "matplotlib", "numpy", "scipy", "cmap"],
+        install_requires=["pyfonts", "matplotlib", "numpy", "scipy", "cmap",
+                          "fluidlab_visualization @ git+https://github.com/hugo-franca/fluidlab_visualization.git@main"],
         # setup_requires=['pytest-runner'],
         # tests_require=['pytest'],
         )
